04fae361723137eedf7d4f9cf78da5876870fc31
Christhofer Vieira <christhofer.vieira@alunos.sc.senac.br>
2022-09-05 21:27:26 -0300

    apicacao heranca

---
+++ src/Heranca.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author christhofer.vieira
+ */
+public class Heranca {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Automovel carro = new Automovel();
+        Automovel moto = new Automovel();
+        
+        carro.setNumPlaca("GRO-9585");
+        carro.setNumPortas(3);
+        carro.setNumRodas(4);
+        carro.setCapacidade(5);
+        
+    }
+    
+}
---
+++ src/Transporte.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author christhofer.vieira
+ */
+public class Transporte {
+    private int capacidade;
+
+    public int getCapacidade() {
+        return capacidade;
+    }
+
+    public void setCapacidade(int capacidade) {
+        this.capacidade = capacidade;
+    }
+    
+    
+}
